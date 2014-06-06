af3767d8a15b3272473c823d684953a9b9a93295
TimmyCP <timmygeo50@gmail.com>
2014-06-06 16:54:12 -0400

    Bot.php
    Bot String etc

---
+++ Source/Add-Ons/Bot/Bot.php
@@ -0,0 +1,21 @@
+<?php
+$this->bot = array(
+	"BotName" => "Bot",
+	"BotMood" => "I'm the Bot Bro!",
+	"BotOnGame" => true,
+	"GlowString" => array(
+		"0",//0
+		"0xFFFFFF",//1
+		"0x0099FF",//2
+		"0x0099FF",//3
+		"0x0099FF",//4
+		"",//5
+		"0x0099FF",//6
+		"",//7
+		"0xFFFFFF",//8
+		"0x000000",//9
+		"0x000000",//10
+	)
+);
+?>
+
