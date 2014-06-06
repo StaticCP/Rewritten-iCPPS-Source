03cb3746b2fe995130f251f02123412b1fe99da7
TimmyCP <timmygeo50@gmail.com>
2014-06-06 17:03:17 -0400

    Create config.php

---
+++ Source/Config/config.php
@@ -0,0 +1,15 @@
+<?php
+$config["ADDRESS"] = '0';
+$config["PORT"] = 6113;
+$config["MAX_CLIENTS"] = 500;//No point of editing that because PHP can't handle even that much
+$config["DEBUG"] = true;
+$config["RAW_STR"] = true;
+$config["RAW_SEPERATOR"] = "%";
+$config["USE_SEND_HANDLER"] = true;
+$config["SEND_HANDLER"] = "s";
+$config["MAX_IN_ROOM"] = 85;
+$config["ALLOW_OTHER_HANDLERS"] = true;
+$config["SMARTBOT"] = false;
+$config["JAIL_SYSTEM"] = true;
+$config["JAIL_ROOM"] = 110; //Jail Room ID
+?>
