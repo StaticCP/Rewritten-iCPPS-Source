59bb3b424af4ef66c872f0b182040a60c417832b
TimmyCP <timmygeo50@gmail.com>
2014-06-06 16:55:47 -0400

    Emotes.php
    SmartBot Functions - Emotes

--- Source/Add-Ons/Bot/Emotes.php
+++ Source/Add-Ons/Bot/Emotes.php
@@ -5,7 +5,7 @@ switch($botemote) {
 	case 2:
 		$phrases = array("You're really happy, aren't you?", ":) to you too!", "I love smiley faces!", "You're happy? So am I!");
 		$message = $phrases[array_rand($phrases)];
-		$objClient->sendData(makeXt("sm", $objClient->intRoomID, 0, "$message"));
+		$objClient->sendData(makeXt("sm", $objClient->intRoomID, 0, $message));
 	break;
 }
 ?>
